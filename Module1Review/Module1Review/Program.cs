eddf5277868467f8c362c4ad55b5c6f3e04d4efc
Sam Shadwell <sshadwell978@gmail.com>
2024-04-30 10:22:52 -0400

    Created file for Module 1 Review exercise

---
+++ Module1Review/Module1Review/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Module1Review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
